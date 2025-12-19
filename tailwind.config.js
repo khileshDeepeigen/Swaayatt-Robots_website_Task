f0ac55b311e4fcffdd449adc1e1f83c6f4557160
kirteeem <kirteem.sharma@deepeigen.com>
2025-12-19 14:29:07 +0530

    Contact Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        rethink: ['"Rethink Sans"'],
+        rethink: ["Rethink Sans"],
       },
     },
   },
